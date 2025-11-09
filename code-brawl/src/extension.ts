911d64da43cacbe2b4384e5c290b9bef15836192
JCassio <josecassio2013@gmail.com>
2025-11-09 17:18:31 -0300

    feat (extension command): submit code

--- code-brawl/src/extension.ts
+++ code-brawl/src/extension.ts
@@ -1,26 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import axios from "axios";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const API_URL = "http://localhost:8080/api/submit";
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Parabéns, sua extensão "coderunner" está ativa!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-brawl" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "coderunner.submitCode",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("Nenhum editor de texto ativo.");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-brawl.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-extension!');
-	});
+      const code = editor.document.getText();
+      const language = editor.document.languageId;
 
-	context.subscriptions.push(disposable);
+      vscode.window.showInformationMessage(`Enviando código ${language}...`);
+
+      try {
+        const response = await axios.post(API_URL, {
+          code: code,
+          language: language,
+        });
+
+        const result = response.data;
+
+        if (result.exitCode === 0) {
+          vscode.window.showInformationMessage("Sucesso!\n" + result.stdout);
+        } else {
+          vscode.window.showErrorMessage(
+            `Erro (Código: ${result.exitCode}):\n` + result.stderr
+          );
+        }
+      } catch (error: any) {
+        console.error(error);
+        vscode.window.showErrorMessage(
+          "Erro ao conectar no backend: " + error.message
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
